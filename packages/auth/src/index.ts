09e0fec4f51277b52d98d0ac215e064cd4f0a626
Panji Gumelar <panjiggm@gmail.com>
2026-01-07 10:56:18 +0700

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@dukcapil-smki/db";
+import * as schema from "@dukcapil-smki/db/schema/auth";
+import { env } from "@dukcapil-smki/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
